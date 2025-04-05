d8839db44a94979aa2b20e1fa943665574dfe92a
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-04-05 22:19:14 +0530

    some frontend work

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import Right from './home/right/right'
 import Left from './home/left/left'
 import Logout from './home/left1/logout'
+import Signup from './components/signup'
 
 const App = () => {
-  return (
-    <div className="flex h-screen">
-       <Logout></Logout>
-       <Left></Left>
-       <Right></Right>
-    </div>
+  return ( 
+    <>
+    {/* // <div className="flex h-screen">
+    //    <Logout></Logout>
+    //    <Left></Left>
+    //    <Right></Right>
+    // </div> */}
+    <Signup />
+    </>
+
   )
 }
 
---
+++ Frontend/src/components/Signup.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+export  default function Signup() {
+  return (
+    <div>
+        <form action="" className="items-center flex flex-col justify-center h-screen">
+           <h1 className="text-xl font-bold mt-10">
+             Create a new {" "}
+             <span className="text-2xl font-bold text-green-500">Account</span>
+           </h1>
+           <h1 className="pb-2">It's free and always will be</h1>
+
+           <div>
+           <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+  {/* <legend className="fieldset-legend">Page details</legend> */}
+
+  {/* full name*/ }
+  <label className="fieldset-label">Yourname</label>
+  <input type="text" className="input" placeholder="Enter your fullname" />
+
+  {/* email */ }
+  
+  <label className="fieldset-label">Email</label>
+  <input type="email" className="input" placeholder="Enter your email" />
+
+
+  {/* password */ }
+  <label className="fieldset-label">Password</label>
+  <input type="password" className="input" placeholder="Enter your Password" />
+
+  {/* confirmed Password */}
+
+  <label className="fieldset-label">Confirmed Password</label>
+  <input type="password" className="input" placeholder="Confirm your password" />
+
+  {/* sign up button */}
+  <input type="submit"  value="Signup" className=" text-white text-xl bg-green-500 mt-3 rounded-lg cursor-pointer text-center"></input>
+   </fieldset>
+
+
+
+   {/* Text and buttons */}
+   <div className="flex flex-col items-center pt-2">
+       <p className="items-center justify-center">Have any account? {" "}
+          <span className="underline text-green-500 cursor-pointer">Login</span>
+        </p>  
+      </div>
+           </div>
+        </form>
+    </div>
+  )
+}
+
+
+
