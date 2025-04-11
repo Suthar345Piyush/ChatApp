bc25aab1e2627b25ccb34c53f44f6927c90ebbc4
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-04-11 07:18:31 +0530

    working on logout

--- Backend/index.js
+++ Backend/index.js
@@ -32,3 +32,5 @@ app.use("/api/user", userRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
--- Frontend/src/home/left1/logout.jsx
+++ Frontend/src/home/left1/logout.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiLogout } from "react-icons/ci";
+import axios from 'axios';
+import Cookies from 'js-cookie';
 
-const Logout = () => {
+export  default function  Logout(){
+  const [loading , setLoading] = useState(false);
+  const handleLogout = async () => {
+     setLoading(true);
+      try{
+         const  res = await axios.post("/api/user/logout");
+         localStorage.removeItem("messsge");
+         Cookies.remove("jwt");
+         setLoading(false);
+         alert("Logout Succussfully");
+      } catch(error){
+         console.log(error);
+      }
+  };
+
+
+  
   return (
     <div className="w-[2%]  bg-green-700 text-white flex flex-col justify-end">
        <button className="cursor-pointer mb-3 hover:bg-gray-400 rounded-lg">
@@ -11,4 +29,4 @@ const Logout = () => {
   )
 }
 
-export default Logout;
+
