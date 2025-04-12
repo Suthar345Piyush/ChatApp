8f668247206443d931859257bd82f94d6d3acf5e
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-04-13 00:47:30 +0530

    solved the issue

--- Frontend/src/home/left/User.jsx
+++ Frontend/src/home/left/User.jsx
@@ -3,13 +3,11 @@ import useConversation from '../../states/useConverstion.js';
 function User({user}) {
   const  {selectedConversation , setSelectedConversation} = useConversation();
   const  isSelected = selectedConversation?._id === user._id;
-  const isOnline = onlineUsers.includes(user._id);
-  console.log(allUsers);
 
   return (
      <div className={`hover:bg-slate-600 duration-300 ${ isSelected ? "bg-slate-700" : ""}`}  onClick={() => setSelectedConversation(user)}>
        <div className="flex space-x-4 px-2 py-2 hover:bg-gray-400 cursor-pointer">
-         <div className={`avatar ${isOnline ? "online" : ""}`}>
+         <div>
             <div className="w-13 rounded-full">
               <img src="https://pbs.twimg.com/profile_images/1905153443184136192/NLepSlSF_400x400.jpg" />
           </div>
